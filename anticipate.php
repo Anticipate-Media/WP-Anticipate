f42221d3c62f11daed9fa93fcfecc3fa72f4657f
Aart Jan van der Linden <mail@aartjan.com>
2026-03-03 12:00:02 +0100

    plugin_file

--- anticipate.php
+++ anticipate.php
@@ -85,6 +85,7 @@ function self_update( $update, array $plugin_data, string $plugin_file, $locales
 		'version' => $new_version_number,
 		'url'     => $new_url,
 		'package' => $new_package,
+		'plugin_file' => 'WP-Anticipate/anticipate.php',
 
 	);
 }
