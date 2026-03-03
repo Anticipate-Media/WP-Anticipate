6e943de815837c0f9afb70986ed72b2b0f19b33b
Aart Jan van der Linden <mail@aartjan.com>
2026-03-03 12:00:11 +0100

    Update anticipate.php

--- anticipate.php
+++ anticipate.php
@@ -3,7 +3,7 @@
 Plugin Name:  Anticipate Plugin
 Plugin URI:   https://anticipate.nl
 Description:  Extra functionaliteiten
-Version:      4.0.13
+Version:      4.0.14
 Author:       Anticipate / Aart Jan
 Author URI:   https://anticipate.nl/
 License:      GPL2
