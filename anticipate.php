0c568678c3478555dfb9ffcecb117432fda86651
Aart Jan van der Linden <mail@aartjan.com>
2026-03-17 15:06:08 +0100

    Css Video

--- anticipate.php
+++ anticipate.php
@@ -3,7 +3,7 @@
 Plugin Name:  Anticipate Plugin
 Plugin URI:   https://anticipate.nl
 Description:  Extra functionaliteiten
-Version:      4.1.5
+Version:      4.1.6
 Author:       Anticipate / Aart Jan
 Author URI:   https://anticipate.nl/
 License:      GPL2
